64c228256d55e02b30a8e539d64e0b78d19382d9
Duncan Marsh <slugbyte@slugbyte.com>
2024-06-28 22:58:52 +0800

    no tabs

--- lua/lackluster/init.lua
+++ lua/lackluster/init.lua
@@ -17,8 +17,8 @@ local tweak = require("lackluster.tweak")
 local highlight = require("lackluster.highlight")
 
 local M = {
-	color = color,
-	dev = dev,
+    color = color,
+    dev = dev,
 }
 
 ---@class LacklusterConfigTweakSyntax
@@ -73,142 +73,142 @@ local USER_CONFIG = nil
 
 --- @type LacklusterConfig
 local default_config = {
-	tweak_syntax = {
-		-- ('default' is default) ('#ffaaff' is a custom colorcode)
-		string = "default",
-		string_escape = "default",
-		comment = "default",
-		builtin = "default", -- builtin modules and functions
-		type = "default",
-		keyword = "default",
-		keyword_return = "default",
-		keyword_exception = "default",
-	},
-	tweak_background = {
-		-- ('default' is default) ('none' is transparent) ('#ffaaff' is a custom hexcode)
-		normal = "default", -- main background
-		menu = "default", -- nvim_cmp, wildmenu ...
-		popup = "default", -- lazy, mason, whichkey ...
-		telescope = "default", -- telescope
-	},
-	disable_plugin = {
-		bufferline = false,
-		cmp = false,
-		dashboard = false,
-		flash = false,
-		git_gutter = false,
-		git_signs = false,
-		headlines = false,
-		lazy = false,
-		lightbulb = false,
-		lsp_config = false,
-		mason = false,
-		mini_diff = false,
-		navic = false,
-		noice = false,
-		notify = false,
-		oil = false,
-		rainbow_delimiter = false,
-		scrollbar = false,
-		telescope = false,
-		todo_comments = false,
-		tree = false,
-		trouble = false,
-		which_key = false,
-		yanky = false,
-	},
+    tweak_syntax = {
+        -- ('default' is default) ('#ffaaff' is a custom colorcode)
+        string = "default",
+        string_escape = "default",
+        comment = "default",
+        builtin = "default", -- builtin modules and functions
+        type = "default",
+        keyword = "default",
+        keyword_return = "default",
+        keyword_exception = "default",
+    },
+    tweak_background = {
+        -- ('default' is default) ('none' is transparent) ('#ffaaff' is a custom hexcode)
+        normal = "default",    -- main background
+        menu = "default",      -- nvim_cmp, wildmenu ...
+        popup = "default",     -- lazy, mason, whichkey ...
+        telescope = "default", -- telescope
+    },
+    disable_plugin = {
+        bufferline = false,
+        cmp = false,
+        dashboard = false,
+        flash = false,
+        git_gutter = false,
+        git_signs = false,
+        headlines = false,
+        lazy = false,
+        lightbulb = false,
+        lsp_config = false,
+        mason = false,
+        mini_diff = false,
+        navic = false,
+        noice = false,
+        notify = false,
+        oil = false,
+        rainbow_delimiter = false,
+        scrollbar = false,
+        telescope = false,
+        todo_comments = false,
+        tree = false,
+        trouble = false,
+        which_key = false,
+        yanky = false,
+    },
 }
 
 local fix_legacy_tweak_typo = function(config)
-	config = config or {}
-	-- TODO: set health check error
-	if config.tweek_background then
-		config.tweak_background = config.tweek_background
-		config.tweek_background = nil
-	end
-	if config.tweek_syntax then
-		config.tweak_syntax = config.tweek_syntax
-		config.tweek_background = nil
-	end
-	return config
+    config = config or {}
+    -- TODO: set health check error
+    if config.tweek_background then
+        config.tweak_background = config.tweek_background
+        config.tweek_background = nil
+    end
+    if config.tweek_syntax then
+        config.tweak_syntax = config.tweek_syntax
+        config.tweek_background = nil
+    end
+    return config
 end
 
 ---configure lackluster with optional settings
 ---@param config ?LacklusterConfig
 M.setup = function(config)
-	config = fix_legacy_tweak_typo(config)
-	config = vim.tbl_deep_extend("keep", config, default_config)
-	USER_CONFIG = config
-	tweak.background(config.tweak_background, theme)
-	tweak.syntax(config.tweak_syntax, theme)
+    config = fix_legacy_tweak_typo(config)
+    config = vim.tbl_deep_extend("keep", config, default_config)
+    USER_CONFIG = config
+    tweak.background(config.tweak_background, theme)
+    tweak.syntax(config.tweak_syntax, theme)
 end
 
 -- apply the colorscheme
 M.load = function(opt)
-	opt = opt or {}
+    opt = opt or {}
 
-	if USER_CONFIG == nil then
-		USER_CONFIG = vim.tbl_deep_extend("force", {}, default_config)
-	end
+    if USER_CONFIG == nil then
+        USER_CONFIG = vim.tbl_deep_extend("force", {}, default_config)
+    end
 
-	local t = theme
-	t.syntax = t.syntax_default
-	vim.o.termguicolors = true
-	vim.g.colors_name = "lackluster"
+    local t = theme
+    t.syntax = t.syntax_default
+    vim.o.termguicolors = true
+    vim.g.colors_name = "lackluster"
 
-	-- official themes
-	if opt.theme == "hack" then
-		t.syntax = t.syntax_hack
-		vim.g.colors_name = "lackluster-hack"
-	end
+    -- official themes
+    if opt.theme == "hack" then
+        t.syntax = t.syntax_hack
+        vim.g.colors_name = "lackluster-hack"
+    end
 
-	if opt.theme == "mint" then
-		t.syntax = t.syntax_mint
-		vim.g.colors_name = "lackluster-mint"
-	end
+    if opt.theme == "mint" then
+        t.syntax = t.syntax_mint
+        vim.g.colors_name = "lackluster-mint"
+    end
 
-	-- expieramental themes
-	if opt.theme == "dark" then
-		t.syntax = t.syntax_dark
-		vim.g.colors_name = "lackluster-dark"
-	end
+    -- expieramental themes
+    if opt.theme == "dark" then
+        t.syntax = t.syntax_dark
+        vim.g.colors_name = "lackluster-dark"
+    end
 
-	if opt.theme == "night" then
-		t.syntax = t.syntax_night
-		vim.g.colors_name = "lackluster-night"
-	end
+    if opt.theme == "night" then
+        t.syntax = t.syntax_night
+        vim.g.colors_name = "lackluster-night"
+    end
 
-	t.syntax = vim.tbl_extend("force", t.syntax, t.syntax_tweak)
+    t.syntax = vim.tbl_extend("force", t.syntax, t.syntax_tweak)
 
-	local dedup_set = {}
-	local highlight_group_list = highlight(t, color)
+    local dedup_set = {}
+    local highlight_group_list = highlight(t, color)
 
-	for _, highlight_group in ipairs(highlight_group_list) do
-		local highlight_spec_list = highlight_group.highlight
-		local is_plugin_enabled = not USER_CONFIG.disable_plugin[highlight_group.plugin_name]
+    for _, highlight_group in ipairs(highlight_group_list) do
+        local highlight_spec_list = highlight_group.highlight
+        local is_plugin_enabled = not USER_CONFIG.disable_plugin[highlight_group.plugin_name]
 
-		if highlight_group.dont_skip or is_plugin_enabled then
-			for _, hl_spec in ipairs(highlight_spec_list) do
-				local hl_name = hl_spec.name
-				if dedup_set[hl_name] then
-					vim.notify("error: duplicate hi_spec :: " .. hl_name, vim.log.levels.ERROR)
-				else
-					dedup_set[hl_name] = true
-					hl_spec.name = nil -- must set to nil so that nvim_set_hl doesn't freak out
+        if highlight_group.dont_skip or is_plugin_enabled then
+            for _, hl_spec in ipairs(highlight_spec_list) do
+                local hl_name = hl_spec.name
+                if dedup_set[hl_name] then
+                    vim.notify("error: duplicate hi_spec :: " .. hl_name, vim.log.levels.ERROR)
+                else
+                    dedup_set[hl_name] = true
+                    hl_spec.name = nil -- must set to nil so that nvim_set_hl doesn't freak out
 
-					-- Filtering out invalid keys here
-					local valid_spec = {}
-					for key, value in pairs(hl_spec) do
-						if key ~= "force" then
-							valid_spec[key] = value
-						end
-					end
-					---@diagnostic disable-next-line: param-type-mismatch
-					vim.api.nvim_set_hl(0, hl_name, valid_spec)
-				end
-			end
-		end
-	end
+                    -- Filtering out invalid keys here
+                    local valid_spec = {}
+                    for key, value in pairs(hl_spec) do
+                        if key ~= "force" then
+                            valid_spec[key] = value
+                        end
+                    end
+                    ---@diagnostic disable-next-line: param-type-mismatch
+                    vim.api.nvim_set_hl(0, hl_name, valid_spec)
+                end
+            end
+        end
+    end
 end
 
 return M
