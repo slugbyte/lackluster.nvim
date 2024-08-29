2b440a09b8a599959351a567243ab9d9c0a89dec
mosaab2020 <mosaabomar@gmail.com>
2024-08-29 07:23:40 +0400

    add mini.jump2d support

--- lua/lackluster/plugin/mini.lua
+++ lua/lackluster/plugin/mini.lua
@@ -10,8 +10,11 @@ return function(theme)
             -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-trailspace.md
             spec.fg("MiniTrailspace", color.orange),
 
-            -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-trailspace.md
+            -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump.md
             spec.co("MiniJump", color.gray1, color.lack),
+
+            -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump2d.md
+            spec.co("MiniJump2dSpot", color.gray1, color.lack)
         },
     }
 end
