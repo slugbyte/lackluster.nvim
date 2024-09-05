a0e585c53e026c1c97be24378e7326935f082921
mosaab2020 <mosaabomar@gmail.com>
2024-09-05 06:07:20 +0400

    add support for mini-statusline

--- lua/lackluster/plugin/mini.lua
+++ lua/lackluster/plugin/mini.lua
@@ -1,5 +1,6 @@
 local spec = require("lackluster.spec")
 local color = require("lackluster.color")
+local color_special = require("lackluster.color-special")
 
 ---@param theme LacklusterTheme
 ---@return LacklusterHighlightGroup
@@ -19,6 +20,21 @@ return function(theme)
             -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-starter.md
             spec.co("MiniStarterQuery", color.blue, color.none),
             spec.co("MiniStarterItemPrefix", color.gray6, color.none),
+
+            -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-statusline.md
+            -- Statusline modes highlights
+            spec.co("MiniStatuslineModeNormal", color.gray7, color_special.statusline, { bold = true }),
+            spec.co("MiniStatuslineModeInsert", color.gray9, color.lack, { bold = true }),
+            spec.co("MiniStatuslineModeVisual", color.black, color.gray9, { bold = true }),
+            spec.co("MiniStatuslineModeCommand", color.gray9, color.lack, { bold = true }),
+            spec.co("MiniStatuslineModeReplace", color.black, color.gray9, { bold = true }),
+            spec.co("MiniStatuslineModeOther", color.gray9, color.lack, { bold = true }),
+
+            -- Other statusline highlights
+            spec.co("MiniStatuslineInactive", color.gray4, color.gray1, { bold = false }),
+            spec.co("MiniStatuslineFilename", color.gray7, color_special.statusline),
+            spec.co("MiniStatuslineFileinfo", color.gray7, color_special.statusline),
+            spec.co("MiniStatuslineDevinfo", color.gray7, color_special.statusline),
         },
     }
 end
